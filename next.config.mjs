3a64709644e85c6198abe2d39aa907080b33fdee
tc0715888 <166673440+tc0715888@users.noreply.github.com>
2025-07-19 02:50:08 +0800

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,21 @@
 /** @type {import('next').NextConfig} */
-
 import setupPWA from 'next-pwa';
 
+// 基础的 Next.js 配置，包含了 i18n 设置
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'zh', 'zh-tw', 'de', 'ru', 'ja', 'fr', 'ko'],
+    defaultLocale: 'en',
+  },
 };
 
+// 使用 next-pwa 包裹基础配置
 const withPWA = setupPWA({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
     register: true,
     skipWaiting: true,
-    buildExcludes: [/manifest\.json$/, /_next\/data/, /_next\/static/],
-    runtimeCaching: [
-      // cache *.css, *.js, *.woff2 files
-      {
-        urlPattern: /^https?.*\.(css|js|woff2)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'assets-cache',
-          expiration: {
-            maxEntries: 200,
-            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-          },
-        }
-      },
-    ],
 });
 
 export default withPWA(nextConfig);
