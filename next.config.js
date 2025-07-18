b65a3c444a8f023bbd75316a27ca21ba5e51fdc1
tc0715888 <166673440+tc0715888@users.noreply.github.com>
2025-07-19 02:54:49 +0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ['en', 'zh', 'zh-tw', 'de', 'ru', 'ja', 'fr', 'ko'],
-    defaultLocale: 'en',
-  },
-}
-
-module.exports = nextConfig 
